77071b86d136d6a89c35d3c4f9ac1be1364a5495
MitchScho <scho.mitch@gmail.com>
2022-04-27 21:36:50 +0000

    create tweet element function and tweet render function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,66 +2,67 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-*/
-[
-{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+ */
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
   },
-  "created_at": 1650848061786
-},
-{
-  "user": {
-    "name": "Descartes",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
-    "handle": "@rd"
-  },
-  "content": {
-    "text": "Je pense , donc je suis"
-  },
-  "created_at": 1650934461786
-}
-]
+];
+
+const createTweetElement = function (tweet) {
+  const $tweet = $(`<article>
+                      <header>
+                        <div class="head-image">
+                          <img src=${tweet.user.avatars}>
+                          <span class="icon-name">${tweet.user.name}</span>            
+                        </div>            
+                        <span class="handle">${tweet.user.handle}</span>
+                      </header>
+                      <p>${tweet.content.text}</p>
+                      <footer>
+                        <output class="day-count">${tweet.created_at}</output>
+                        <div class="foot-icon">
+                          <i class="fa fa-flag" aria-hidden="true"></i>
+                          <i class="fa fa-heart" aria-hidden="true"></i>
+                          <i class="fa fa-bolt" aria-hidden="true"></i>
+                        </div>
+                      </footer>
+                    </article>`);
+                          
+
+  return $tweet;
+};
+
+// const $tweet = createTweetElement(tweetdata);
+// console.log("tweet", $tweet);
+// $(".tweet-container").append($tweet);
+
+const renderTweet = function (data) {
+  for (const item of data) {
+    console.log("data item", item);
+    $(".tweet-container").prepend(createTweetElement(item));
+  }
+};
 
 $(document).ready(function () {
-
-  const createTweetElement = function (data) {
-    
-    const $tweet = $(`<section class="tweet-container">
-        <article>
-          <header class="article-head">
-            <div>
-              <i${user.avatars}></i>
-              <span class="icon-name">${user.name}</span>            
-            </div>            
-            <span class="handle">${user.handle}</span>
-          </header>
-          <p>${content.text}</p>
-          <footer class="article-foot">
-            <output class="day-count">${created_at}</output>
-            <div id="foot-icon">
-              <i class="fa fa-thumbs-up" aria-hidden="true"></i>
-              <i class="fa fa-heart" aria-hidden="true"></i>
-              <i class="fa fa-bolt" aria-hidden="true"></i>
-            </div>
-          </footer>
-        </article>
-      </section>`);
-    return $tweet;
-  }
-
-
-  const renderTweet = function (data) {
-    let newTweet = [];
-    for (item in data) {
-      newTweet.append(createTweetElement(item));
-    }
-    $('tweet-container').append(newTweet);
-  }
-})
-
+  renderTweet(data);
+});
