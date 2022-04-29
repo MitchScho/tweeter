56dd0051cf6c05b627e144521fd6eabfdeca493d
MitchScho <scho.mitch@gmail.com>
2022-04-29 21:34:45 +0000

    dynamic design changes and final bug repairs for submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,34 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
-
-// Uses data passed in through tweet parameter and returns html structured data
+// Uses data passed in through tweet parameter and returns secure html structured data
 const createTweetElement = function (data) {
   const escape = function (str) {
     let tweet = document.createElement("tweet");
@@ -39,17 +9,13 @@ const createTweetElement = function (data) {
                       <header>
                         <div class="head-image">
                           <img src=${data.user.avatars}>
-                          <span class="icon-name">${
-                            data.user.name
-                          }</span>            
+                          <span class="icon-name">${data.user.name}</span>            
                         </div>            
                         <span class="handle">${data.user.handle}</span>
                       </header>
                       <p>${escape(data.content.text)}</p>
                       <footer>
-                        <output class="day-count">${timeago.format(
-                          data.created_at
-                        )}</output>
+                        <output class="day-count">${timeago.format(data.created_at)}</output>
                         <div class="foot-icon">
                           <i class="fa fa-flag" aria-hidden="true"></i>
                           <i class="fa fa-retweet" aria-hidden="true"></i>
@@ -77,7 +43,6 @@ $(document).ready(function () {
     const inputText = $("#tweet-text").val();
     
     if (inputText.length > 140) {
-      
       $("#error").html(`<div class="error-flex">
                           <i class="fa fa-thumbs-down" aria-hidden="true"></i>
                           <span>! Sorry you have exceeded your character allowance !</span>
@@ -106,7 +71,6 @@ $(document).ready(function () {
           $(this).trigger("reset");
         },
         error: (error) => {
-          
         },
       });
     }
@@ -118,8 +82,7 @@ $(document).ready(function () {
       success: (data) => {
         renderTweet(data);
       },
-      error: (error) => {
-        
+      error: (error) => { 
       },
     });
   };
